430195c8ca79a688aa2c9eec74e2c35414bb7fde
ayssis <161905626+dryersheets@users.noreply.github.com>
2024-09-22 15:10:40 -0700

    added script for player and camera
    Player can now jump and go left and right. The camera now follows the player.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    public float jump;
+    public bool isJumping;
+    private float Move;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+     rb = GetComponent<Rigidbody2D>();   
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+        //Jump power
+        if(Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            //This is for a debug, will not affect game
+            Debug.Log("jump");
+
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false; //if player is collided with object (ground) then means Not jumping.
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
     }
 }
